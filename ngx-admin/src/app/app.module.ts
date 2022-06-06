65e0de0573534cd896c91ac1f0ca04fff94cbcc8
Анастасия <Angelice92@mail.ru>
2022-06-06 22:31:25 +0300

    add table and button

--- ngx-admin/src/app/app.module.ts
+++ ngx-admin/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ngx-admin/src/app/components/main/main.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import {DataSource} from '@angular/cdk/collections';
+import {Observable, ReplaySubject} from 'rxjs';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079},
+];
+
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  ngOnInit() {}
+
+  displayedColumns: string[] = ['title', 'color', 'attachments'];
+  dataToDisplay = [...ELEMENT_DATA];
+
+  dataSource = new ExampleDataSource(this.dataToDisplay);
+
+  addData() {
+    const randomElementIndex = 1;
+    this.dataToDisplay = [...this.dataToDisplay, ELEMENT_DATA[randomElementIndex]];
+    this.dataSource.setData(this.dataToDisplay);
+  }
+
+  removeData() {
+    this.dataToDisplay = this.dataToDisplay.slice(0, -1);
+    this.dataSource.setData(this.dataToDisplay);
+  }
+  editData() {
+    this.dataToDisplay = this.dataToDisplay.slice(0, -1);
+    this.dataSource.setData(this.dataToDisplay);
+  }
+}
+
+class ExampleDataSource extends DataSource<PeriodicElement> {
+  private _dataStream = new ReplaySubject<PeriodicElement[]>();
+
+  constructor(initialData: PeriodicElement[]) {
+    super();
+    this.setData(initialData);
+  }
+
+  connect(): Observable<PeriodicElement[]> {
+    return this._dataStream;
+  }
+
+  disconnect() {}
+
+  setData(data: PeriodicElement[]) {
+    this._dataStream.next(data);
+  }
+
+
+}
